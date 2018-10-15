e36ed22ee667d794edb33e3a0d12e045275bcd1e
İbrahim UÇAR <ucribrahim@gmail.com>
2018-10-15 16:32:37 +0300

    Update watchservice.sh

--- watchservice.sh
+++ watchservice.sh
@@ -1,23 +1,20 @@
-## watchservice.sh
-
 #!/bin/bash
-#Author: Ibrahim Ucar
-#Mail: ucribrahim@gmail.com
-#Website: lifeoverlinux.com
+# Author: Ibrahim Ucar
+# Mail: ucribrahim@gmail.com
 
-#-- About this script.
-#-- This script watch a service process, if the process of service stops then script sends a email to the user -
-#-- that let the user know that a service (e.g firefox) stopped. This script just an example, you can watch any services.
+# This script watches a service process if the process of service stops then script sends an email to the user that
+# let the user know that a service (e.g. Firefox) stopped. This script just an example, you can watch any services or
+# configure as you wish.
 
-#-- You can set this script to CRON to make this script works every minute, second, hour, etc.
+# You can set this script to CRON to make this script works every second, minute, hour, etc.
 
 
-# Mail information of users. As you defined the following mail address, you also need to configure ssmtp service.
-# Otherwise you'll not be able to send mail!
+# Define user mail address to the downline. You also need to configure "ssmtp" service in your system.
+# Otherwise script will not be able to send e-mail!
 
 email="username@domain.com"
 
-# Rest of shell script commands.
+# Rest of the shell script commands.
 
 pid=$(ps -e | pgrep firefox | wc -l | xargs echo -n)
 
@@ -25,11 +22,11 @@ echo "I'm checking the process of (Firefox Web Browser)... Please wait a second.
 
 if [ $pid -eq 0 ]; then
         echo
-        echo "Firefox process doesn't exist. Sending e-mail to the user."
-        echo "Firefox process doesn't exist. This is a information that Firefox service stopped." | ssmtp $email
+        echo "Firefox process doesn't exist. I'm sending an e-mail to the user."
+        echo "Firefox process doesn't exist. This is a warning message that Firefox service stopped." | ssmtp $email
 
 else
         echo
-        echo "The process of ( Firefox Web Browser ) exist! It means firefox still working."
+        echo "The process of ( Firefox Web Browser ) exist! It means Firefox service working."
 fi
 
