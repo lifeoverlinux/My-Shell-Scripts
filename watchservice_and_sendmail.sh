50a929355900b9877710e724b750f0fc00bb795f
İbrahim UÇAR <ucribrahim@gmail.com>
2018-10-17 23:30:53 +0300

    Update watchservice_and_sendmail.sh

--- watchservice_and_sendmail.sh
+++ watchservice_and_sendmail.sh
@@ -3,24 +3,27 @@
 # Written in: Debian
 # Mail: ucribrahim@gmail.com
 
+# --- ABOUT THIS SCRIPT ---
+
 # This script watches a service process if the process of service stops then script sends an email to the user that
 # let the user know that a service (e.g. Firefox) stopped. This script just an example, you can watch any services or
 # configure as you wish.
 
-# You can set this script to CRON to make this script works every second, minute, hour, etc.
+# You can set this script to CRON to make this script works every minute, hour, day, etc.
 
-
-# Define user mail address to the downline. You also need to configure "ssmtp" service in your system.
-# Otherwise script will not be able to send e-mail!
+# --- STEPS --- 
+# FIRST: Install "ssmtp" service and then configure it. Make sure that you can send mail with "ssmtp" command line.
+# SECOND: Define a mail address to the downline. Otherwise script will not send e-mail! 
 
 email="username@domain.com"
 
-# Rest of the shell script commands.
-
+# Get the process of the "firefox" service.
 pid=$(ps -e | pgrep firefox | wc -l | xargs echo -n)
 
+# Simple echo massage.
 echo "I'm checking the process of (Firefox Web Browser)... Please wait a second."
 
+# Here, I'm checking the variable that if process equal to "0" number or not equal. 
 if [ $pid -eq 0 ]; then
         echo
         echo "Firefox process doesn't exist. I'm sending an e-mail to the user."
@@ -30,4 +33,3 @@ else
         echo
         echo "The process of ( Firefox Web Browser ) exist! It means Firefox service working."
 fi
-
