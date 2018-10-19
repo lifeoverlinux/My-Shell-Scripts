d285c00c01507f1376458c3dc08c96ef4bbfb8de
İbrahim UÇAR <ucribrahim@gmail.com>
2018-10-19 23:01:48 +0300

    Update disk_space_check_sendmail.sh

--- disk_space_check_sendmail.sh
+++ disk_space_check_sendmail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: Ibrahim Ucar
-# Written in: Centos Server 7
+# Written in: Centos Server 7  ( Should work many GNU/Linux, FreeBSD systems)
 # Date: 25.10.2018
 # Mail: ucribrahim@gmail.com
 
