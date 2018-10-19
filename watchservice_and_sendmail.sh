b340ce1622e0e71e180b454eb272697764f2064d
İbrahim UÇAR <ucribrahim@gmail.com>
2018-10-19 21:17:26 +0300

    Update watchservice_and_sendmail.sh

--- watchservice_and_sendmail.sh
+++ watchservice_and_sendmail.sh
@@ -4,7 +4,7 @@
 # Date: 19.10.2018
 # Mail: ucribrahim@gmail.com
 
-# --- ABOUT THIS SCRIPT ---
+# -------ABOUT THIS SCRIPT------- #
 
 # This script watches a service process if the process of service stops then script sends an email to the user that
 # let the user know that a service (e.g. Firefox) stopped. This script just an example, you can watch any services or
@@ -12,9 +12,10 @@
 
 # You can set this script to CRON to make this script works every minute, hour, day, etc.
 
-# --- STEPS --- 
-# FIRST: Install "ssmtp" service and then configure it. Make sure that you can send mail with "ssmtp" command line.
-# SECOND: Define a mail address to the downline. Otherwise script will not send e-mail! 
+# ------------STEPS------------- #
+# First: Install "ssmtp" service and then configure it. Make sure that you can send mail with "ssmtp" command line.
+# Second: Define a mail address to the downline. Otherwise script will not send e-mail! 
+# ------------STEPS------------- #
 
 email="username@domain.com"
 
@@ -24,7 +25,7 @@ pid=$(ps -e | pgrep firefox | wc -l | xargs echo -n)
 # Simple echo massage.
 echo "I'm checking the process of (Firefox Web Browser)... Please wait a second."
 
-# IF conditions.
+# IF conditions.)
 if [ $pid -eq 0 ]; then
         echo
         echo "Firefox process doesn't exist. I'm sending an e-mail to the user."
